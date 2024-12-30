c314af92ead4f294909261f72d3234f4c2abe060
Kamil Kozik <128065731+Kamil955@users.noreply.github.com>
2024-12-30 10:41:59 +0000

    Creating website

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Example data
+data = [
+    {"id": 1, "name": "Kamil", "age": 22},
+    {"id": 2, "name": "Kacper", "age": 24},
+    {"id": 3, "name": "Andrzej", "age": 35},
+]
+
+# Home page
+@app.route('/')
+def home():
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
